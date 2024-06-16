939dbaaaddddd40b96482392347fa90c2a3868e4
Alexander-Tengborg <Landervich@Gmail.com>
2024-06-16 18:52:34 +0200

    Remove username min length requirement

--- src/utils/user.utils.ts
+++ src/utils/user.utils.ts
@@ -3,7 +3,7 @@ import { isEmail, isStrongPassword } from "validator";
 export const validateUserData = (email: string, username: string, password: string) : boolean => {
     if(
         !email || !isEmail(email) ||
-        !username || username.length < 5 ||
+        !username || username.length == 0 ||
         !password || !isStrongPassword(password)
     ) {
         return false;
